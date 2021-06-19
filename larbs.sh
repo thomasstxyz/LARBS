9adf179375138a40866d1743eafe5dfc8309b255
thomasstxyz <thomas@thomasst.xyz>
2021-06-19 16:02:09 +0200

    change to thomasstxyz's dotfiles and dwm

--- larbs.sh
+++ larbs.sh
@@ -14,8 +14,8 @@ while getopts ":a:r:b:p:h" o; do case "${o}" in
 	*) printf "Invalid option: -%s\\n" "$OPTARG" && exit 1 ;;
 esac done
 
-[ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
-[ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/LukeSmithxyz/LARBS/master/progs.csv"
+[ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/thomasstxyz/dotfiles.git"
+[ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/thomasstxyz/LARBS/master/progs.csv"
 [ -z "$aurhelper" ] && aurhelper="yay"
 [ -z "$repobranch" ] && repobranch="master"
 
